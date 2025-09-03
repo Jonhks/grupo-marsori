060cc5f9816e46dd1042788e1a8879b58c27543a
Jonhks <jonhgx@gmail.com>
2025-09-02 19:31:32 -0600

    Update

--- src/views/ProyectoDetalle.tsx
+++ src/views/ProyectoDetalle.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import proyectos from "../proyectos.json";
 import SliderDinamico from "../components/SliderDinamico";
 
@@ -6,6 +6,7 @@ const ProyectoDetalle = () => {
   const location = useLocation();
   const { id } = useParams();
   const proyecto = location.state?.proyectoSeleccionado;
+  const navigate = useNavigate();
 
   if (!proyecto) {
     return <p>Proyecto no encontrado {id}</p>;
@@ -20,6 +21,12 @@ const ProyectoDetalle = () => {
               key={proyecto.id}
               className="max-w-[70vw]"
             >
+              <button
+                className="bg-black text-white font-montserrat font-medium text-md px-4 py-2 rounded hover:bg-gray-400 transition-all mb-4"
+                onClick={() => navigate(-1)} // Regresa a la página anterior
+              >
+                Atrás
+              </button>
               <h2 className="text-lg md:text-3xl text-center font-bold mb-4">
                 {proyecto.titulo}
               </h2>
