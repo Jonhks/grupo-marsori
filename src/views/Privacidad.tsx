0c528c6f7ff67342fffa12567bde992b3b6b2376
Jonhks <jonhgx@gmail.com>
2025-09-04 19:41:28 -0600

    Update/ cambiando aviso

--- src/views/Privacidad.tsx
+++ src/views/Privacidad.tsx
@@ -1,224 +1,140 @@
 const Privacidad: React.FC = () => {
   return (
     <div className="w-full flex flex-col items-center py-12 px-4 bg-white">
-      <h1 className="text-2xl md:text-3xl font-bold text-center mb-6">
-        AVISO DE PRIVACIDAD. AVISO DE PROTECCIÓN DE DATOS.{" "}
+      <h1 className="text-2xl md:text-3xl font-bold text-center mb-8">
+        AVISO DE PRIVACIDAD
       </h1>
-      <div className=" w-full md:w-[60%] md:px-15">
-        <h2 className="text-xl font-semibold mb-4 text-left">
-          A. Declaración de privacidad.
-        </h2>
-      </div>
+
+      <p className="mb-4 font-bold">"GRUPO MARSORI, S.A. de C.V.".</p>
       <div className="max-w-4xl text-left text-gray-700 font-montserrat text-md leading-relaxed">
         <p className="mb-4">
-          <strong>GRUPO MARSORI, S.A. de C.V.</strong>, sus filiales y
-          subsidiarias (en lo sucesivo El Prestador), con domicilio en Juventino
-          Rosas no 114 colonia Guadalupe Inn, Álvaro Obregón, CP 01020 en la
-          CDMX, por medio de la presente le hace de conocimiento que se
-          compromete a proteger su información otorgada a nosotros.
-        </p>
-        <p className="mb-4">
-          De conformidad a la Ley Federal de Protección de Datos Personales en
-          Posesión de los Particulares, en los artículos 3, Fracciones II y VII,
-          15 y 33, así como la denominación del capítulo II, del Título Segundo,
-          de la Ley Federal de Transparencia y Acceso a la Información Pública
-          Gubernamental, le informamos que toda su información personal en
-          nuestras bases de datos no está a la venta ni disponible para su
-          comercialización con terceros. Se entenderá que el titular consiente
-          tácitamente el tratamiento de sus datos siempre y cuando no nos
-          manifieste su oposición.
+          En cumplimiento con lo establecido en la Ley Federal de Protección de
+          Datos Personales en Posesión de los Particulares, Grupo Marsori, S.A.
+          de C.V. (en lo sucesivo “Grupo Marsori”), con domicilio en Insurgentes
+          Sur 1853 Piso 5 Colonia Guadalupe Inn, Delegación Álvaro Obregón, CP
+          01020, CDMX es responsable del tratamiento y protección de sus datos
+          personales.
         </p>
         <div className=" w-full md:w-[80%] md:px-15">
           <h2 className="text-xl font-semibold mb-4 text-left w-full">
-            B. Finalidad y tratamiento de la Información Personal.
+            1. Datos personales que recabamos
           </h2>
         </div>
-
-        <p className="mb-4 md:p-8">
-          1.- La información personal que nos proporciona será utilizada por El
-          Prestador para las siguientes finalidades: proveer los servicios y
-          productos que ha solicitado; notificarle sobre nuevos servicios y/o
-          productos que tenga relación con los ya contratados o adquiridos;
-          comunicarle sobre cambios en los mismos; elaborar estudios y programas
-          que son necesarios para determinar hábitos de consumo; realizar
-          evaluaciones periódicos de nuestros productos y servicios a efecto de
-          mejorar la calidad de los mismos; evaluar la calidad del servicio que
-          brindamos, y en general, para dar cumplimiento a las obligaciones que
-          hemos contraído con usted
-        </p>
-        <p className="mb-4">
-          Para las finalidades antes mencionadas, requerimos obtener los
-          siguientes datos personales:
-        </p>
         <ul className="list-disc pl-5 text-gray-700 font-montserrat text-md font-semibold">
-          <li>Nombre completo</li>
-          <li>Edad</li>
-          <li>Estado civil</li>
-          <li>Sexo</li>
-          <li>Teléfono fijo y/o celular</li>
-          <li>Correo electrónico</li>
-          <li>Firma autógrafa</li>
-          <li>Dirección</li>
-          <li>Nivel de escolaridad</li>
-          <li>RFC y/o CURP</li>
           <li>
-            Contabilidad, Información financiera, empresarial y comercial.
+            Datos de identificación: nombre, domicilio, teléfono, correo
+            electrónico, firma, RFC, CURP, identificación oficial.
+          </li>
+          <li>
+            Datos laborales: puesto, empresa, referencias, historial laboral (en
+            caso de candidatos o empleados).
+          </li>
+          <li>
+            Datos financieros o patrimoniales: información bancaria, CLABE,
+            cuentas, datos fiscales (únicamente cuando sea necesario para el
+            cumplimiento de obligaciones legales y contractuales).
           </li>
         </ul>
-        <p className="my-4">
-          La cual manifiesta ser cierta, pertinente correcta y actualizada para
-          los fines para los cuales fueron recabados. Por otra parte hacemos de
-          su conocimiento que sus datos podrán ser transferidos a entidades del
-          mismo grupo de interés de las empresas nacionales, con el objeto de
-          cumplir con las finalidades para las cuales ha proporcionado sus
-          datos. Propósitos específicos: el proporcionar el servicio adecuado.
+        <p className="mb-4 md:p-8">
+          No recabamos datos personales sensibles sin consentimiento expreso.
         </p>
-        <p className="mb-4">
-          En caso de que no obtengamos su oposición expresa para que sus datos
-          personales sean transferidos en la forma y términos antes descrita,
-          entenderemos que ha otorgado su consentimiento en forma tácita para
-          ello.
-        </p>
-        <p className="mb-4 p-0 md:p-8">
-          2.- Finalmente la información que nos proporciona podrá ser tratada en
-          los casos que expresamente autoriza el artículo 10 la Ley Federal de
-          Protección de Datos Personales en Posesión de los Particulares
-        </p>
-        <div className="w-full md:w-[80%] md:px-15">
-          <h2 className="text-xl font-semibold mb-4 text-left">
-            C. Uso de Datos Personales Sensibles.
+        <div className=" w-full md:w-[80%] md:px-15">
+          <h2 className="text-xl font-semibold mb-4 text-left w-full">
+            2. Finalidades del tratamiento
           </h2>
         </div>
         <p className="mb-4">
-          Se consideran datos personales sensibles aquellos datos personales que
-          afecten a la esfera más íntima de su titular, o cuya utilización
-          indebida pueda dar origen a discriminación o conlleve un riesgo grave
-          para éste. En particular, se consideran sensibles aquellos que puedan
-          revelar aspectos como origen racial, étnico, estado de salud presente
-          y futuro, información genética, creencias religiosas, filosóficas y
-          morales, afiliación sindical, opiniones políticas, preferencias
-          sexuales y si dicha información es recopilada por parte de los
-          clientes sólo podrá ser utilizada para mejorar o satisfacer las
-          necesidades de los clientes y los servicios que se les proporcionan a
-          los mismos.
+          Sus datos personales serán utilizados para las siguientes finalidades
+          primarias:
         </p>
-        <div className=" w-[90%] md:px-15">
-          <h2 className="text-xl font-semibold mb-4 text-left">
-            D. Limitación al Uso o Divulgación de Información Personal.
+        <ul className="list-disc pl-5 text-gray-700 font-montserrat text-md font-semibold">
+          <li>Proveer los servicios y productos que ha solicitado.</li>
+          <li>Dar cumplimiento a obligaciones contractuales.</li>
+          <li>Facturación y cobranza.</li>
+          <li>Evaluar la calidad del servicio.</li>
+          <p className="m-2 text-light  font-normal">
+            Y para las siguientes finalidades secundarias:
+          </p>
+          <li>Envío de información comercial, promociones y publicidad.</li>
+          <li>Elaboración de perfiles de clientes y estadísticas.</li>
+        </ul>
+        <p className="mb-4">
+          El titular puede negarse a que sus datos se usen para finalidades
+          secundarias enviando un correo a: info@marsori.com .
+        </p>
+        <div className=" w-full md:w-[80%] md:px-15">
+          <h2 className="text-xl font-semibold mb-4 text-left w-full">
+            3. Transferencia de datos
+          </h2>
+        </div>
+        <p className="mb-4">Sus datos podrán ser compartidos únicamente con:</p>
+        <ul className="list-disc pl-5 text-gray-700 font-montserrat text-md font-semibold">
+          <li>Autoridades competentes que lo requieran por mandato legal.</li>
+          <li>
+            Terceros proveedores de servicios relacionados estrictamente con la
+            operación de Grupo Marsori, S.A. de C.V. (por ejemplo, mensajería,
+            asesoría legal, auditoría o sistemas, servicios de ingeniería etc).
+          </li>
+        </ul>
+        <p className="mb-4">
+          En ningún caso se comercializarán sus datos personales.
+        </p>
+        <div className=" w-full md:w-[80%] md:px-15">
+          <h2 className="text-xl font-semibold mb-4 text-left w-full">
+            4. Derechos ARCO
           </h2>
         </div>
         <p className="mb-4">
-          Usted podrá limitar en cualquier momento la información proporcionada
-          a El Prestador respecto del tratamiento señalado en los numerales 3 y
-          4 del inciso B), mediante una solicitud que deberá de ser presentada
-          por escrito al domicilio ubicado en- Juventino Rosas no 114 colonia
-          Guadalupe Inn, Álvaro Obregón, CP 01020 en la CDMX , de las 9:00 horas
-          a las 18:00 horas de lunes a viernes, o al correo
-          electrónico----------------------. Con la finalidad de proteger su
-          confidencialidad, El Prestador enviará respuesta a su solicitud por
-          escrito al domicilio o correo electrónico que haya proporcionado en su
-          escrito de solicitud a elección de ustedes.
+          Usted tiene derecho a Acceder, Rectificar, Cancelar u Oponerse al
+          tratamiento de sus datos personales (Derechos ARCO), así como a
+          revocar el consentimiento otorgado.
         </p>
-        <div className=" w-[100%] md:px-15">
-          <h2 className="text-xl font-semibold mb-4 text-left">
-            E. Del ejercicio de los Derechos de Acceso, Rectificación,
-            Cancelación y Oposición
+        <p className="mb-4">
+          Para ejercer estos derechos deberá enviar una solicitud a través del
+          correo electrónico: info@marsori.com o en el domicilio señalado, con
+          atención al área de Protección de Datos Personales.
+        </p>
+        <p className="mb-4">La solicitud deberá contener:</p>
+        <ul className="list-disc pl-5 text-gray-700 font-montserrat text-md font-semibold">
+          <li>Nombre completo y medio de contacto.</li>
+          <li>Identificación oficial.</li>
+          <li>
+            Descripción clara de los datos respecto de los cuales busca ejercer
+            sus derechos.
+          </li>
+        </ul>
+        <div className=" w-full md:w-[80%] md:px-15">
+          <h2 className="text-xl font-semibold mb-4 text-left w-full">
+            5. Medidas de seguridad
           </h2>
         </div>
         <p className="mb-4">
-          El acceso, rectificación, cancelación u oposición respecto a la
-          información que proporcione a El Prestador señalados en la Ley Federal
-          de Protección de Datos Personales en Posesión de los Particulares, lo
-          podrá hacer mediante entrega de la solicitud que dirija a El
-          Prestador, en términos de lo establecido en el inciso D. anterior,
-          misma que se le responderá por escrito o mediante correo electrónico
-          en los términos ahí señalados.
+          Grupo Marsori, S.A. de C.V. ha implementado medidas de seguridad
+          físicas, técnicas y administrativas para proteger sus datos personales
+          contra daño, pérdida, alteración, destrucción, uso indebido, acceso no
+          autorizado o tratamiento indebido.
         </p>
-        <div className=" w-[100%] md:px-15">
-          <h2 className="text-xl font-semibold mb-4 text-left">
-            F. Control y Seguridad de Información Personal.
+        <div className=" w-full md:w-[80%] md:px-15">
+          <h2 className="text-xl font-semibold mb-4 text-left w-full">
+            6. Cambios al aviso de privacidad
           </h2>
         </div>
         <p className="mb-4">
-          El Prestador se compromete a tomar las medidas necesarias para
-          proteger la información recopilada, utilizando tecnologías de
-          seguridad y procedimientos de control en el acceso, uso o divulgación
-          de su información personal proporcionada en servidores ubicados en
-          Centros de Datos que cuentan con controles de acceso limitado. Para
-          transacciones en línea, utilizamos también tecnologías de seguridad
-          que protegen la información personal que nos sea transmitida a través
-          de los diversos medios electrónicos. Sin embargo, ningún sistema de
-          seguridad o de transmisión de datos del cual la empresa no tenga el
-          control absoluto y/o tenga dependencia con internet puede garantizar
-          que sea totalmente seguro.
+          Grupo Marsori se reserva el derecho de modificar o actualizar este
+          Aviso de Privacidad en cualquier momento, para la atención de reformas
+          legislativas o políticas internas. Las modificaciones estarán
+          disponibles en nuestro domicilio y en la página web
         </p>
-        <div className=" w-[100%] md:px-15">
-          <h2 className="text-xl font-semibold mb-4 text-left">
-            G. Medios electrónicos cookies.
-          </h2>
-        </div>
-        <p className="mb-4">
-          En el supuesto de que usted utilice medios electrónicos en relación a
-          sus datos personales se generarán a efecto de proporcionarle un mejor
-          servicio de cookies.
-        </p>
-        <p className="mb-4">
-          Los cookies es un término informático relacionado con la información
-          que guarda un servidor sobre un usuario en su equipo, se almacena en
-          el disco duro de su equipo y se utilizan para determinar sus
-          preferencias cuando se conecta a los servicios de nuestros sitios, así
-          como para rastrear determinados comportamientos o actividades llevadas
-          a cabo por usted dentro de nuestros sitios.
-        </p>
-        <p className="mb-4">
-          En algunas secciones de nuestro sitio requerimos que el cliente tenga
-          habilitados las cookies ya que algunas de las funcionalidades
-          requieren éstas para trabajar. Los cookies nos permiten: a)
-          Reconocerlo al momento de entrar a nuestros sitios y ofrecerle un
-          servicio personalizado, b) Conocer la configuración personal del sitio
-          especificada por usted, mencionando enunciativa y no limitativamente
-          la banda ancha que selecciona al momento de ingresar al Home Page de
-          nuestros sitios, de tal forma que sabemos qué tipo de información es
-          recomendable descargar, c) Calcular el tamaño de nuestros usuarios y
-          medir los parámetros de tráfico, pues cada navegador que obtiene
-          acceso a nuestros sitios adquiere un cookie que se usa para determinar
-          la frecuencia de uso y las secciones de los sitios visitados,
-          reflejando así sus hábitos y preferencias, información que nos es útil
-          para mejorar el contenido, los titulares y las promociones para los
-          usuarios. Las cookies también nos ayudan a rastrear algunas
-          actividades, por ejemplo, en algunas de las encuestas que lanzamos en
-          línea, podemos utilizar cookies para detectar si el usuario ya ha
-          llenado la encuesta y evitar desplegarla nuevamente. El botón de
-          “ayuda” que se encuentra en la barra de herramientas de la mayoría de
-          los navegadores, le dirá cómo evitar aceptar nuevas cookies, cómo
-          hacer que el navegador le notifique cuando recibe una nueva cookie o
-          cómo deshabilitar todas las cookies. Sin embargo, las cookies le
-          permitirán tomar ventaja de las características más benéficas que le
-          ofrecemos, por lo que le recomendamos que las deje activadas.
-        </p>
-        <div className=" w-[100%] md:px-15">
-          <h2 className="text-xl font-semibold mb-4 text-left">
-            H. Cambios a esta declaración de privacidad.
-          </h2>
-        </div>
-        <p className="mb-4">
-          El Prestador podrá en cualquier momento actualizar esta declaración de
-          privacidad. En caso de que se produzcan cambios sustanciales a esta
-          declaración, lo referenciaremos a la siguiente página de internet.
-        </p>
-        <p className="mb-4">
-          Por lo anterior, le sugerimos visitar periódicamente esta declaración
-          de privacidad para estar enterado de cualquier actualización.
-        </p>
-        <p className="mb-4">
-          Acepto que la información personal que proporciono a El prestador se
-          trate conforme a lo establecido en este Aviso de privacidad, por lo
-          que me doy por enterado del contenido del Aviso de Privacidad.
-        </p>
-        <p className="mb-4">
-          Acepto que mis datos personales se transmitan para enviarme
-          información de productos y servicios de asociados y de terceros,
-          conforme a lo establecido en el presente Aviso de privacidad.
+        <a
+          className="text-blue-500 hover:underline"
+          href="https://grupomarsori.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          www.grupomarsori.com
+        </a>
+        <p className="my-4 font-semibold text-center">
+          Fecha de última actualización: 04 de septiembre del 2025
         </p>
       </div>
     </div>
